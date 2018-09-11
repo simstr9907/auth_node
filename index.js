b43cd8b91bccf7b536483df790a8e6e3760d6145
Simon Strandh <simon@strandh.one>
2018-09-11 14:22:21 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,76 @@
+//Importera moduler
+const express = require("express");
+const fs = require("fs");
+const bcrypt = require("bcryptjs");
+const bodyParser = require("body-parser");
+//Applikations variable
+const app = express();
+
+
+app.use(("/public"),express.static(__dirname + "/public"));
+
+app.get("/createuser", function(req,res){
+
+let user ={};
+user.email = "demo@krati.se";
+user.id = Date.now();
+let tmpPassword = "1234";
+
+bcrypt.genSalt(12, function(err, salt){
+    if(err) throw err;
+    console.log(salt);
+    bcrypt.hash(tmpPassword, salt, function(err, hash){
+        if(err) throw err;
+        console.log(hash);
+        res.send("Hello new user");
+
+        user.password = hash;
+        saveUser(user);
+    });
+    
+});
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+let port = process.env.port || 2006;
+
+app.listen(port, function(){
+    console.log("App lyssnar på port: " + port);
+});
+
+
+
+//Hjälp funktioner
+
+function saveUser(userObj){
+    //const users = Array.from(require("./.data/users.json"));
+    let users = "";
+    fs.readFile("./.data/users.json", function(err, data){
+        let tmpData = data.toString();
+        tmpData = JSON.parse(tmpData);
+        users = Array.from(tmpData);
+        
+    
+    users.push(userObj);
+
+    fs.writeFile("./.data/users.json", JSON.stringify(users,null,2), function(err){
+        if (err) console.log("No user added");
+        else console.log("User added");
+    });
+
+}); 
+
+}
